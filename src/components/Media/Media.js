900637185d7ad8de38d003724932db69abc68e6b
AhmedTamer2003 <ahmedmarketing7575@gmail.com>
2024-09-25 09:39:36 +0300

    Initial commit

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -7,16 +7,16 @@ const Media = () => {
       <h2 className="media-title">Follow Me On</h2>
       <div className="media-icons">
         {/* Replace # with actual URLs to your social media */}
-        <a href="https://facebook.com" className="media-link" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.facebook.com/eltkny.el3arby" className="media-link" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-facebook-f"></i>
         </a>
         <a href="https://twitter.com" className="media-link" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-twitter"></i>
         </a>
-        <a href="https://linkedin.com" className="media-link" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/ahmed-tamer-304706302/" className="media-link" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-linkedin-in"></i>
         </a>
-        <a href="https://instagram.com" className="media-link" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.instagram.com/eltkny.el3arby2003/" className="media-link" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-instagram"></i>
         </a>
       </div>
